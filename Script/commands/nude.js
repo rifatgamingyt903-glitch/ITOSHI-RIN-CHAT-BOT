91475e9ee63c47594cbbd6b934e334cd703a885f
rifatgamingyt903-glitch <rifatgamingyt903@gmail.com>
2025-09-17 17:25:09 +0600

    Create nude.js

---
+++ Script/commands/nude.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+
+module.exports.config = {
+    name: "animegirl", // change command name
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "MMRifat",
+    description: "Sends a random anime girl picture",
+    commandCategory: "fun",
+    usages: "",
+    cooldowns: 5
+};
+
+module.exports.run = async function({ api, event }) {
+    var link = [
+        "https://i.imgur.com/XYz9PZ3.jpg",
+        "https://i.imgur.com/7C7k9Tj.jpg",
+        "https://i.imgur.com/m5nbqQZ.jpg",
+        "https://i.imgur.com/bqdfmM8.jpg"
+    ];
+    
+    let img = link[Math.floor(Math.random() * link.length)];
+    
+    api.sendMessage({
+        body: "「 Here is your random pic 😻 」",
+        attachment: await global.utils.getStreamFromURL(img)
+    }, event.threadID, event.messageID);
+};
